8fcecf70ad92631e5cebc8a14fa4ec4198e51d8b
Mikesam <1627072456@qq.com>
2019-04-30 14:59:36 +0800

    放弃使用中文了

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ cd /home/container
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo "欢迎使用聚乐云我的世界(Minecraft)基岩服务端(Bedrock)镜像"
-echo "更多精彩内容，尽在 https://open.clayun.com"
+echo "welcome to use clayun's docker image"
+echo "more info in https://open.clayun.com"
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
