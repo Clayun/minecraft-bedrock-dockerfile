fc34b5dc2c997f7ecb584aa5785d1741d762368c
Mikesam <1627072456@qq.com>
2019-04-30 14:48:18 +0800

    就必须带点英文才不乱码么

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cd /home/container
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo "欢迎使用聚乐云我的世界基岩服务端镜像"
+echo "欢迎使用聚乐云我的世界(Minecraft)基岩服务端(Bedrock)镜像"
 echo "更多精彩内容，尽在 https://open.clayun.com"
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
